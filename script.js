5e23ed34d7aec1498fb63f8c45e68fc24a63d379
Mark Zihao Yu <yuzihao.1997@gmail.com>
2025-02-10 17:48:44 +1300

    11

---
+++ script.js
@@ -0,0 +1,12 @@
+function rollDice4(){
+    return Math.floor(Math.random()* 4) + 1
+}
+function rollDice6(){
+    return Math.floor(Math.random()* 6) + 1
+}
+function rollDice10(){
+    return Math.floor(Math.random()* 10) + 1
+}
+function rollDice32(){
+    return Math.floor(Math.random()* 32) + 1
+}
