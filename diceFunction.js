3cf8a98e35ae66bb9b01cbfc12bf326b6fba0402
Mark Zihao Yu <yuzihao.1997@gmail.com>
2025-02-10 22:54:35 +1300

    unitTestsAdded

--- diceFunction.js
+++ diceFunction.js
@@ -1,3 +1,4 @@
+//function
 function rollDice4(){
     return Math.floor(Math.random()* 4) + 1
 }
@@ -10,3 +11,7 @@ function rollDice10(){
 function rollDice32(){
     return Math.floor(Math.random()* 32) + 1
 }
+console.log(rollDice4())
+console.log(rollDice6())
+console.log(rollDice10())
+console.log(rollDice32())
